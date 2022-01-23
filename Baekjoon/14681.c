79a8b617804ee4967c734496b4db25eee6421ff7
JooYoungEom <eomjy1226@gmail.com>
2022-01-23 15:06:32 +0900

    [14681]사분면 고르기

---
+++ Baekjoon/14681.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+
+int main(void)
+{
+	int x, y = 0;
+	
+	scanf("%d", &x);
+	scanf("%d", &y);
+	if (x > 0 && y > 0) {
+		printf("1");
+	}
+	else if (x < 0 && y>0) {
+		printf("2");
+	}
+	else if (x < 0 && y < 0) {
+		printf("3");
+	}
+	else {
+		printf("4");
+	}
+	
+}
