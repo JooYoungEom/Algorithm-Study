4a0531ec01109aff381eec3d3e74b15d741dd07d
JooYoungEom <eomjy1226@gmail.com>
2022-02-13 20:08:26 +0900

    [10952]A+B - 5

---
+++ Baekjoon/10952.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+	int a, b, c;
+	while (1) {
+		scanf("%d %d", &a, &b);
+		if (a == 0 && b == 0) {
+			break;
+		}
+		else {
+			printf("%d\n", a + b);
+		}
+	}
+}
