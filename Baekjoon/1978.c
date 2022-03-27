a6745f4d21c0569007dd94eb93541d4a44ab468f
JooYoungEom <eomjy1226@gmail.com>
2022-03-27 16:53:57 +0900

    [1978]소수 찾기

---
+++ Baekjoon/1978.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    int n;
+    int num, count = 0;
+
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &num);
+        for (int j = 2; j <= num; j++)
+        {
+            if (num == j)
+                count++;
+            if (num % j == 0)
+                break;
+        }
+    }
+
+    printf("%d", count);
+
+    return 0;
+}
