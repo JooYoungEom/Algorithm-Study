853dd9be14712ca0c88430dc9ea983025a724cff
JooYoungEom <eomjy1226@gmail.com>
2022-03-27 16:24:31 +0900

    [10250]ACM 호텔

---
+++ 10250.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+
+int main() {
+
+	int t, h, w, n;  
+	int count;    
+	scanf("%d", &t);
+
+	for (int i = 0; i < t; i++) {
+		count = 1;    
+		scanf("%d %d %d", &h, &w, &n);
+		while (n > h) {     
+			n -= h;
+			count++;
+
+			 
+		}
+		printf("%d%02d\n", n, count);   
+	}
+
+
+	return 0;
+}
