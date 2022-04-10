383eb624a70dd7ab48b0f4dac07c628e454dccab
JooYoungEom <eomjy1226@gmail.com>
2022-04-10 15:38:35 +0900

    [3053]택시 기하학

---
+++ Baekjoon/3053.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#define PI 3.14159265359
+
+int main(void)
+{
+    int r;
+    double result1, result2;
+
+    scanf("%d", &r);
+    result1 = (double)r * r * PI; //유클리드 기하학
+    result2 = (double)r * r * 2;  //택시 기하학
+
+    printf("%.6lf\n%.6lf", result1, result2);
+    return 0;
+}
+
